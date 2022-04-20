97ee16b15b2f456b08392e9f3ea6376e052865a7
jmhickman <jon@zer0metasec.com>
2022-04-20 07:51:05 -0700

    Update level-7.sol

--- level-7.sol
+++ level-7.sol
@@ -8,3 +8,9 @@ contract ForceReceive {
         selfdestruct(_f);
     }
 }
+
+// I'll admit, this challenge had me stumped, and I had to look at the ethernaut repo to be nudged into getting the answer. I tried various 
+// forms of `call` at first, but I found that the documentation was correct and that the transaction would revert because the fallback wasn't
+// payable. I also tried funding as an EOA. I even thought maybe the challenge was a tricksy hobbit and that I needed to fund the deployment of
+// the instance (that function _is_ payable, and _does_ pass along the value much like a `call` does) but the value went to the level deployer,
+// not the deployed level. Sigh Anyway, interesting challenge.
