ca8f1edd5aab5392812a34f8286b6f000d855ec8
jmhickman <jon_hickman@proton.me>
2022-04-29 09:52:32 -0700

    Update level-22.fsx

--- level-22.fsx
+++ level-22.fsx
@@ -78,11 +78,17 @@ checkPoolBalances ()
 (*
 The integer math portion aside, getting prices or any sort of data from any single source is a massive attack vector in smart contracts.
 
-You can clearly see from this example, that someone with a lot of capital could manipulate the price in one fell swoop, and cause any applications relying on it to use the the wrong price.
+You can clearly see from this example, that someone with a lot of capital could manipulate the price in one fell swoop, and cause any 
+applications relying on it to use the the wrong price.
 
-The exchange itself is decentralized, but the price of the asset is centralized, since it comes from 1 dex. This is why we need oracles. Oracles are ways to get data into and out of smart contracts. We should be getting our data from multiple independent decentralized sources, otherwise we can run this risk.
+The exchange itself is decentralized, but the price of the asset is centralized, since it comes from 1 dex. This is why we need oracles. 
+Oracles are ways to get data into and out of smart contracts. We should be getting our data from multiple independent decentralized sources, 
+otherwise we can run this risk.
 
-Chainlink Data Feeds are a secure, reliable, way to get decentralized data into your smart contracts. They have a vast library of many different sources, and also offer secure randomness, ability to make any API call, modular oracle network creation, upkeep, actions, and maintainance, and unlimited customization.
+Chainlink Data Feeds are a secure, reliable, way to get decentralized data into your smart contracts. They have a vast library of many different
+sources, and also offer secure randomness, ability to make any API call, modular oracle network creation, upkeep, actions, and maintainance, and 
+unlimited customization.
 
-Uniswap TWAP Oracles relies on a time weighted price model called TWAP. While the design can be attractive, this protocol heavily depends on the liquidity of the DEX protocol, and if this is too low, prices can be easily manipulated.
+Uniswap TWAP Oracles relies on a time weighted price model called TWAP. While the design can be attractive, this protocol heavily depends on the
+liquidity of the DEX protocol, and if this is too low, prices can be easily manipulated.
 *)
